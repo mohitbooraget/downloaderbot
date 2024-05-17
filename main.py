80834bebba97f2dfa06091d11088498f9d5e1bcf
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-05-18 00:09:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ import os
 bot = Client("bot",
              bot_token= "7166882529:AAE6OEz5R_ky9OCERVNSYO9YDOU4I8lKb-I",
              api id= "23291931"
-             api_hash= "4b11dd648188731fb7c9bc8083e8791c)
+             api_hash= "4b11dd648188731fb7c9bc8083e8791c")
 
 
 @bot.on_message(filters.command(["start"]) & filters.user(ADMINS))
