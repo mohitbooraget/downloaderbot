fe614fb83cb4a80894999c69b5da16c3bf3c814c
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-05-18 00:19:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ import os
 
 bot = Client("bot",
              bot_token= "7166882529:AAE6OEz5R_ky9OCERVNSYO9YDOU4I8lKb-I",
-             api id= 23291931,
+             api_id= 23291931,
              api_hash= "4b11dd648188731fb7c9bc8083e8791c")
 
 
