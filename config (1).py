1c825d21327a48d3ce2767297ba4aa75f44f5d38
mohitbooraget <137650003+mohitbooraget@users.noreply.github.com>
2024-05-18 00:26:09 +0530

    Add files via upload

---
+++ config (1).py
@@ -0,0 +1,14 @@
+import os
+
+API_ID = API_ID = 23291931
+
+API_HASH = os.environ.get("API_HASH", "4b11dd648188731fb7c9bc8083e8791c")
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7078804290:AAEOgiUjc9tzMte1SBepRZII6cRizz6Bysg")
+
+PASS_DB = int(os.environ.get("PASS_DB", "721"))
+
+OWNER = int(os.environ.get("OWNER", 6594402123))
+
+LOG = -1002054601495
+
